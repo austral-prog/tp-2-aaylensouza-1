bc4da08d6041aaecad7c6d0b59eda55dd7984ecd
aaylensouza-1 <aylensouza3@gmail.com>
2024-08-16 21:55:39 -0300

    entrega 3

--- change.py
+++ change.py
@@ -1,3 +1,8 @@
 def change():
     expense = 23.75
     money = 100
+    print(f'Ingresar gasto:\n' + f'{expense}')
+    print(f'Dinero recibido\n' + f'{money}\n')
+    print('Vuelto\n')
+    print(f'Pesos:\n' + f'{int(money - expense)}')
+    print(f'Centavos:\n' + f'{int(money / 4)}')
